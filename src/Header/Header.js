dfff4c140d09445080cfe763e9735dd70e6e2fb6
hossain909 <70032159+hossain909@users.noreply.github.com>
2021-06-28 20:59:22 +0600

    All done

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home" >Movie World world</Navbar.Brand>
+        <Navbar.Brand href="#home" >Movie World</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
