a7c239be7e3821000fdbd542507fb13ce5324a58
hossain909 <70032159+hossain909@users.noreply.github.com>
2021-06-28 20:58:22 +0600

    All done

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,21 +5,21 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home" >Movie World</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link href="#link">Top Rated</Nav.Link>
-              <Nav.Link href="#link">New Arrival</Nav.Link>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
+      <Container>
+        <Navbar.Brand href="#home" >Movie World world</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link href="#link">Top Rated</Nav.Link>
+            <Nav.Link href="#link">New Arrival</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
